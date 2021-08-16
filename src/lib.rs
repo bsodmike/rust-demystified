a29ea3806b74fed51da6b34e5268d85bac42a2ce
Michael de Silva <michael@mwdesilva.com>
2021-08-16 13:01:33 +0530

    FIX: Correct logic for Iterator
    This was incorrectly only returning the first item in the vector.

--- src/lib.rs
+++ src/lib.rs
@@ -23,9 +23,9 @@ fn shoes_in_size_iterable(shoes: MyVec<Vec<Shoe>>, shoe_size: u32) -> MyVec<Vec<
     shoes.iter().filter(|s| s.size == shoe_size).collect()
 }
 
-fn shoes_in_size_into_iter(shoes: MyVec<Vec<Shoe>>, shoe_size: u32) -> MyVec<Vec<Shoe>> {
-    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
-}
+// fn shoes_into_iter(shoes: MyVec<Vec<Shoe>>, shoe_size: u32) -> MyVec<Vec<Shoe>> {
+//     shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+// }
 
 #[derive(PartialEq, Debug, Copy, Clone)]
 struct MyVec<T>(T);
@@ -56,25 +56,27 @@ impl Iterator for MyVecIter<'_, Vec<Shoe>> {
     type Item = Shoe;
     fn next(&mut self) -> Option<Self::Item> {
         let inner_vec = &self.vector.0;
-        if self.cur <= inner_vec.len() {
+        if self.cur < inner_vec.len() {
             self.cur += 1;
 
             println!("cur: {:?}", self.cur);
-            return Some(inner_vec[0]);
+            return Some(inner_vec[self.cur - 1]);
         } else {
             return None
         }
     }
 }
 
-impl IntoIterator for MyVec<Vec<Shoe>> {
-    type Item = Shoe;
-    type IntoIter = std::vec::IntoIter<Self::Item>;
+// impl IntoIterator for MyVec<Vec<Shoe>> {
+//     type Item = Shoe;
+//     type IntoIter = std::vec::IntoIter<Self::Item>;
 
-    fn into_iter(self) -> Self::IntoIter {
-        self.0.into_iter()
-    }
-}
+//     fn into_iter(self) -> Self::IntoIter {
+//         let inner_vec = self.0;
+
+//         return inner_vec.into_iter();
+//     }
+// }
 
 // Implemented to call `filter`
 impl FromIterator<Shoe> for MyVec<Vec<Shoe>> {
@@ -96,54 +98,54 @@ impl FromIterator<Shoe> for MyVec<Vec<Shoe>> {
 mod tests {
     use super::*;
 
+    // #[test]
+    // fn add_into_iter() {
+    //     let shoes = MyVec(vec![
+    //         Shoe {
+    //             size: 10,
+    //             id: 0
+    //         },
+    //         Shoe {
+    //             size: 13,
+    //             id: 1,
+    //         },
+    //         Shoe {
+    //             size: 10,
+    //             id: 2,
+    //         },
+    //     ]);
+
+    //     let in_my_size = shoes_into_iter(shoes, 10);
+
+    //     assert_eq!(
+    //         in_my_size,
+    //         MyVec(vec![
+    //             Shoe {
+    //                 size: 10,
+    //                 id: 0,
+    //             },
+    //             Shoe {
+    //                 size: 10,
+    //                 id: 2,
+    //             },
+    //         ])
+    //     );
+    // }
+
     #[test]
-    fn add_into_iter() {
+    fn filters_by_size_wrapped_iter() {
         let shoes = MyVec(vec![
             Shoe {
                 size: 10,
-                id: 0
+                id: 100
             },
             Shoe {
                 size: 13,
-                id: 1,
+                id: 101,
             },
             Shoe {
                 size: 10,
-                id: 2,
-            },
-        ]);
-
-        let in_my_size = shoes_in_size_into_inter(shoes, 10);
-
-        assert_eq!(
-            in_my_size,
-            MyVec(vec![
-                Shoe {
-                    size: 10,
-                    id: 0,
-                },
-                Shoe {
-                    size: 10,
-                    id: 2,
-                },
-            ])
-        );
-    }
-
-    #[test]
-    fn filters_by_size_wrapped() {
-        let shoes = MyVec(vec![
-            Shoe {
-                size: 10,
-                id: 0
-            },
-            Shoe {
-                size: 13,
-                id: 1,
-            },
-            Shoe {
-                size: 10,
-                id: 2,
+                id: 102,
             },
         ]);
 
@@ -154,11 +156,11 @@ mod tests {
             MyVec(vec![
                 Shoe {
                     size: 10,
-                    id: 0,
+                    id: 100,
                 },
                 Shoe {
                     size: 10,
-                    id: 2,
+                    id: 102,
                 },
             ])
         );
