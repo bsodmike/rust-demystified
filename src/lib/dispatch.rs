93862a3037783dd8cadef41757c61774caf251ba
Michael de Silva <michael@mwdesilva.com>
2022-04-08 15:36:08 +0530

    Add error output

--- src/lib/dispatch.rs
+++ src/lib/dispatch.rs
@@ -2,6 +2,30 @@ pub trait Hei {
     fn hei(&self);
 
     fn weird() {};
+// error[E0038]: the trait `dispatch::Hei` cannot be made into an object
+//   --> src/lib/dispatch.rs:19:20
+//    |
+// 19 | pub fn say_hei(s: &dyn Hei) {
+//    |                    ^^^^^^^ `dispatch::Hei` cannot be made into an object
+//    |
+// note: for a trait to be "object safe" it needs to allow building a vtable to allow the call to be resolvable dynamically; for more information visit <https://doc.rust-lang.org/reference/items/traits.html#object-safety>
+//   --> src/lib/dispatch.rs:4:8
+//    |
+// 1  | pub trait Hei {
+//    |           --- this trait cannot be made into an object...
+// ...
+// 4  |     fn weird() {};
+//    |        ^^^^^ ...because associated function `weird` has no `self` parameter
+// help: consider turning `weird` into a method by giving it a `&self` argument
+//    |
+// 4  |     fn weird(&self) {};
+//    |              +++++
+// help: alternatively, consider constraining `weird` so it does not apply to trait objects
+//    |
+// 4  |     fn weird() where Self: Sized {};
+//    |                +++++++++++++++++
+
+
 }
 
 impl Hei for &str {
