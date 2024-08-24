64a7b112d5ce9c98e417e283e354c29d4fd6805d
Michael de Silva <mike.cto@securecloudsolutions.io>
2024-08-25 00:24:51 +0530

    FIX: remove unnecessary cloning of HashMap

--- src/string_search/core.rs
+++ src/string_search/core.rs
@@ -1,5 +1,6 @@
 use anyhow::anyhow;
 use anyhow::Error;
+use rand::seq::IteratorRandom;
 use std::collections::HashMap;
 
 pub struct Entries(pub HashMap<String, f64>);
@@ -13,33 +14,25 @@ impl Entries {
         self.0.len()
     }
 
-    pub fn add(&mut self, text: String, amount: f64) {
+    pub fn add(&mut self, info: (String, f64)) {
+        let (text, amount) = info;
         let mut existing_data = self.0.clone();
         existing_data.insert(text, amount);
 
         self.0 = existing_data;
     }
 
-    pub fn add_many(&mut self, items: HashMap<&str, f64>) {
-        let new: HashMap<String, f64> = items
-            .into_iter()
-            .map(|el| (el.0.to_string(), el.1))
-            .collect();
+    pub fn add_many<'a>(&mut self, items: impl IntoIterator<Item = (&'a str, f64)>) {
+        let new: HashMap<String, f64> =
+            items.into_iter().map(|(k, v)| (k.to_string(), v)).collect();
         self.0 = new;
     }
 
     pub fn search(&mut self, input: &str, amount: f64) -> Result<(String, f64, f64), Error> {
         let text = input.to_string();
-
-        // println!("Number of items to search: {}", entries.0.len());
-
         let needle = text;
-        let haystack: Vec<String> = self
-            .0
-            .clone()
-            .into_iter()
-            .map(|el| el.0.to_string())
-            .collect();
+        let haystack: Vec<String> = self.0.iter().map(|el| el.0.to_string()).collect();
+        log::debug!("Number of items to search: {}", self.0.len());
 
         if haystack.contains(&needle) {
             if let Some(cost) = self.0.get(&needle) {
