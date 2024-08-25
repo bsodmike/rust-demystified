227a816bd4adc54dfdcc7690ca3e8e51f058773d
Michael de Silva <mike.cto@securecloudsolutions.io>
2024-08-25 18:03:43 +0530

    FIX: further cleanup for previous change

--- src/bin/string-search-heaptrack.rs
+++ src/bin/string-search-heaptrack.rs
@@ -13,33 +13,31 @@ extern crate tutorials;
 
 #[tokio::main]
 pub async fn main() -> Result<(), Error> {
-    static PHRASE_COUNT: i32 = 50;
+    static PHRASE_COUNT: i32 = 10_000_000;
     let rng_phrases = phrases(PHRASE_COUNT);
-    // let items: HashMap<&str, f64> = rng_phrases
-    //     .iter()
-    //     .map(|phrase| (phrase.as_str(), rng_amount()))
-    //     .collect();
-
-    // let hm_keys = &items.clone().into_keys().collect::<Vec<&str>>();
-
-    // let mut entries = Entries::new();
-    // entries.add_many(black_box(items));
-
-    // // Success, the buyer gets an instant match!
-    // let _ = black_box(
-    //     entries
-    //         .search(hm_keys[0], 120.00)
-    //         .expect("Unable to search through entries!"),
-    // );
-
     if let Ok(phrases) = rng_phrases {
-        println!(
-            "Phrases: {}",
-            phrases
-                .iter()
-                .map(|el| { format!("\"{}\", ", el) })
-                .collect::<String>()
+        let items: HashMap<&str, f64> = phrases
+            .iter()
+            .map(|phrase| (phrase.as_str(), rng_amount()))
+            .collect();
+
+        let hm_keys = &items.clone().into_keys().collect::<Vec<&str>>();
+
+        let mut entries = Entries::new();
+        entries.add_many(black_box(items));
+
+        // Success, the buyer gets an instant match!
+        let _ = black_box(
+            entries
+                .search(hm_keys[0], 120.00)
+                .expect("Unable to search through entries!"),
         );
+
+        // let phrase_text = phrases
+        //     .iter()
+        //     .map(|el| format!("\"{}\", ", el))
+        //     .collect::<String>();
+        println!("Phrases generated: {}", &phrases.len());
     }
 
     Ok(())
