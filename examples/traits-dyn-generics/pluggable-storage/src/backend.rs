f9f7e54296f6ebd50ed017e80717514bfc964e17
Michael de Silva <mike.cto@securecloudsolutions.io>
2024-09-04 09:52:38 +0530

    Remove unused imports

--- examples/traits-dyn-generics/pluggable-storage/src/backend.rs
+++ examples/traits-dyn-generics/pluggable-storage/src/backend.rs
@@ -1,12 +1,10 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use std::fmt::Debug;
 use std::future::Future;
-use std::ops::{Deref, DerefMut};
-use std::path::PathBuf;
 
 pub(crate) mod disk;
 pub(crate) mod s3;
 
 pub trait Backend: Debug {
-  fn persist(&mut self, data: &[u8]) -> impl Future<Output = Result<()>> + Send;
+    fn persist(&mut self, data: &[u8]) -> impl Future<Output = Result<()>> + Send;
 }
--- examples/traits-dyn-generics/pluggable-storage/src/backend/s3.rs
+++ examples/traits-dyn-generics/pluggable-storage/src/backend/s3.rs
@@ -9,40 +9,40 @@ use super::Backend;
 #[derive(Default, Builder, Debug)]
 #[builder(setter(into))]
 pub struct S3Config {
-  bucket: String,
-  file_name: String,
+    bucket: String,
+    file_name: String,
 }
 
 #[allow(dead_code)]
 #[derive(Debug)]
 pub struct S3Backend {
-  config: S3Config,
+    config: S3Config,
 }
 
 impl S3Backend {
-  fn new(config: S3Config) -> Self {
-    Self { config }
-  }
+    fn new(config: S3Config) -> Self {
+        Self { config }
+    }
 
-  pub fn bucket(&self) -> String {
-    self.config.bucket.to_string()
-  }
+    pub fn bucket(&self) -> String {
+        self.config.bucket.to_string()
+    }
 
-  pub fn file_name(&self) -> String {
-    self.config.file_name.to_string()
-  }
+    pub fn file_name(&self) -> String {
+        self.config.file_name.to_string()
+    }
 }
 
 pub trait S3BackendTrait {
-  fn new(config: S3Config) -> Self;
+    fn new(config: S3Config) -> Self;
 }
 
 impl S3BackendTrait for S3Backend {
-  // NOTE: This is a provided method via the Trait `S3BackendTrait`
-  fn new(config: S3Config) -> Self {
-    // NOTE: this is an associated method on `S3Backend`
-    Self::new(config)
-  }
+    // NOTE: This is a provided method via the Trait `S3BackendTrait`
+    fn new(config: S3Config) -> Self {
+        // NOTE: this is an associated method on `S3Backend`
+        Self::new(config)
+    }
 }
 
 #[derive(Debug)]
@@ -50,43 +50,43 @@ pub struct S3BackendBuilder<T>(T);
 
 // Example on implemeting Deref for an inner type.
 impl<T> Deref for S3BackendBuilder<T> {
-  type Target = T;
+    type Target = T;
 
-  fn deref(&self) -> &Self::Target {
-    &self.0
-  }
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 impl<T> DerefMut for S3BackendBuilder<T> {
-  fn deref_mut(&mut self) -> &mut Self::Target {
-    &mut self.0
-  }
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
 }
 
 impl<T> S3BackendBuilder<T>
 where
-  T: S3BackendTrait,
+    T: S3BackendTrait,
 {
-  pub fn new(config: S3Config) -> Self {
-    Self(T::new(config))
-  }
+    pub fn new(config: S3Config) -> Self {
+        Self(T::new(config))
+    }
 
-  pub fn build(self) -> T {
-    self.0
-  }
+    pub fn build(self) -> T {
+        self.0
+    }
 }
 
 impl Backend for S3Backend {
-  async fn persist(&mut self, data: &[u8]) -> Result<()> {
-    let bucket_name = &self.bucket();
-    let file_name = &self.file_name();
+    async fn persist(&mut self, data: &[u8]) -> Result<()> {
+        let bucket_name = &self.bucket();
+        let file_name = &self.file_name();
 
-    unimplemented!();
+        unimplemented!();
 
-    // if let Err(err) = _ {
-    //   return Err(err).with_context(|| format!("Failed to persist to {:?}", self.path));
-    // }
+        // if let Err(err) = _ {
+        //   return Err(err).with_context(|| format!("Failed to persist to {:?}", self.path));
+        // }
 
-    Ok(())
-  }
+        // Ok(())
+    }
 }
