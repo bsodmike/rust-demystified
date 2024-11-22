db62a9b266630e4db8ae213ea14ffc3e80d42874
Michael de Silva <mike.cto@securecloudsolutions.io>
2024-11-22 13:23:49 +0530

    Add more examples on pinning

---
+++ src/bin/pin-2.rs
@@ -0,0 +1,53 @@
+#![allow(unused_variables)]
+#![allow(dead_code)]
+
+use anyhow::Error;
+
+#[derive(Debug)]
+struct Test {
+    a: String,
+    b: *const String,
+}
+
+impl Test {
+    fn new(txt: &str) -> Self {
+        Test {
+            a: String::from(txt),
+            b: std::ptr::null(),
+        }
+    }
+
+    fn init(&mut self) {
+        let self_ref: *const String = &self.a;
+        self.b = self_ref;
+    }
+
+    fn a(&self) -> &str {
+        &self.a
+    }
+
+    fn b(&self) -> &String {
+        assert!(
+            !self.b.is_null(),
+            "Test::b called without Test::init being called first"
+        );
+        unsafe { &*(self.b) }
+    }
+}
+
+// https://rust-lang.github.io/async-book/04_pinning/01_chapter.html#pinning-in-detail
+// Demonstrates the issue of self-referential types without the use of pinning leading to UB.
+pub fn main() -> Result<(), Error> {
+    let mut test1 = Test::new("test1");
+    test1.init();
+    let mut test2 = Test::new("test2");
+    test2.init();
+
+    println!("a: {}, b: {}", test1.a(), test1.b());
+    std::mem::swap(&mut test1, &mut test2);
+    test1.a = "I've totally changed now!".to_string();
+
+    println!("a: {}, b: {}", test2.a(), test2.b());
+
+    Ok(())
+}
