9aac87f26832cdcacc8a2de53defdd97ff78f6f4
Michael de Silva <mike.cto@securecloudsolutions.io>
2024-12-01 12:38:51 +0530

    Formatting

--- examples/traits-dyn-generics/trait-objects-without-downcast-generics/src/main.rs
+++ examples/traits-dyn-generics/trait-objects-without-downcast-generics/src/main.rs
@@ -11,9 +11,9 @@ use std::ops::{Deref, DerefMut};
 struct Dwarf {}
 
 impl Display for Dwarf {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "I may be small but the ladies don't complain!")
-  }
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "I may be small but the ladies don't complain!")
+    }
 }
 
 #[derive(Debug)]
@@ -24,107 +24,107 @@ struct Human {}
 
 #[derive(Debug)]
 enum Thing {
-  Sword,
-  Trinket,
+    Sword,
+    Trinket,
 }
 
 trait Enchanter: std::fmt::Debug {
-  fn competency(&self) -> f64;
+    fn competency(&self) -> f64;
 
-  fn enchant(&self, thing: &mut Thing) {
-    let probability_of_success = self.competency();
-    let spell_is_successful = rand::thread_rng().gen_bool(probability_of_success); // <1>
+    fn enchant(&self, thing: &mut Thing) {
+        let probability_of_success = self.competency();
+        let spell_is_successful = rand::thread_rng().gen_bool(probability_of_success); // <1>
 
-    print!("{:?} mutters incoherently. ", self);
-    if spell_is_successful {
-      println!("The {:?} glows brightly.", thing);
-    } else {
-      println!(
-        "The {:?} fizzes, \
+        print!("{:?} mutters incoherently. ", self);
+        if spell_is_successful {
+            println!("The {:?} glows brightly.", thing);
+        } else {
+            println!(
+                "The {:?} fizzes, \
              then turns into a worthless trinket.",
-        thing
-      );
-      *thing = Thing::Trinket {};
+                thing
+            );
+            *thing = Thing::Trinket {};
+        }
     }
-  }
 }
 
 impl Enchanter for Dwarf {
-  fn competency(&self) -> f64 {
-    0.5 // <2>
-  }
+    fn competency(&self) -> f64 {
+        0.5 // <2>
+    }
 }
 impl Enchanter for Elf {
-  fn competency(&self) -> f64 {
-    0.95 // <3>
-  }
+    fn competency(&self) -> f64 {
+        0.95 // <3>
+    }
 }
 impl Enchanter for Human {
-  fn competency(&self) -> f64 {
-    0.8 // <4>
-  }
+    fn competency(&self) -> f64 {
+        0.8 // <4>
+    }
 }
 
 /// Holds the log value, stored on the heap.
 #[derive(Debug)]
 struct LogRecord {
-  pub text: String,
+    pub text: String,
 }
 
 impl LogRecord {
-  fn new() -> Self {
-    Self {
-      text: String::default(),
+    fn new() -> Self {
+        Self {
+            text: String::default(),
+        }
     }
-  }
 }
 
 trait Recordable: Debug {
-  fn text(&self) -> String;
+    fn text(&self) -> String;
 
-  fn set_text(&mut self, _: String);
+    fn set_text(&mut self, _: String);
 }
 
 impl Recordable for LogRecord {
-  fn set_text(&mut self, value: String) {
-    self.text = value;
-  }
+    fn set_text(&mut self, value: String) {
+        self.text = value;
+    }
 
-  fn text(&self) -> String {
-    self.text.to_string()
-  }
+    fn text(&self) -> String {
+        self.text.to_string()
+    }
 }
 
 fn log_enchanted<'a, T: Any + Debug, U: Recordable>(value: &T, l: &'a mut U) -> &'a U {
-  let value_any = value as &dyn Any;
+    let value_any = value as &dyn Any;
 
-  // Try to convert our value its concrete type
-  match value_any.downcast_ref::<Dwarf>() {
-    Some(_) => {
-      l.set_text("Default text within the recorder".to_string());
+    // Try to convert our value its concrete type
+    match value_any.downcast_ref::<Dwarf>() {
+        Some(_) => {
+            l.set_text("Default text within the recorder".to_string());
 
-      l
+            l
+        }
+        _ => l,
     }
-    _ => l,
-  }
 }
 
 fn main() {
-  let mut it = Thing::Sword;
+    let mut it = Thing::Sword;
 
-  let d = Dwarf {};
-  let e = Elf {};
-  let h = Human {};
+    let d = Dwarf {};
+    let e = Elf {};
+    let h = Human {};
 
-  let party: Vec<&dyn Enchanter> = vec![&d, &h, &e]; // <5>
+    let party: Vec<&dyn Enchanter> = vec![&d, &h, &e]; // <5>
 
-  // This is a contrived example to try and downcase from a leaked Box, to its concrete type without breaking Safety.
-  let d = Dwarf {};
-  let mut l = LogRecord::new();
-  let r: &LogRecord = log_enchanted(&d, &mut l);
-  assert_eq!("Default text within the recorder".to_string(), r.text());
+    // This is a contrived example to try and downcase from a leaked Box, to its concrete type without breaking Safety.
+    let d = Dwarf {};
+    let mut l = LogRecord::new();
+    let r: &LogRecord = log_enchanted(&d, &mut l);
+    assert_eq!("Default text within the recorder".to_string(), r.text());
 
-  let spellcaster = party.choose(&mut rand::thread_rng()).unwrap();
+    let spellcaster = party.choose(&mut rand::thread_rng()).unwrap();
 
-  spellcaster.enchant(&mut it);
+    spellcaster.enchant(&mut it);
 }
